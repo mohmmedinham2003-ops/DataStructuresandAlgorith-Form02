0eaba112a92318f3b1366277e17b094dcb72f152
Mohammed Inham <mohmmedinham2003@gmail.com>
2026-07-13 20:50:33 +0530

    Define Node class and main method in Demo.java

--- Demo.java
+++ Demo.java
@@ -1,1 +1,27 @@
+import java.util.*;
+class Node(){
+		int data;
+		Node next;
+		Node(int data){
+		this.data = data;	
+		}
+	
+}
 
+
+
+
+
+
+
+
+
+
+class Demo(){
+		public static void main(String args[]){
+			
+			
+			}
+	
+	
+}
